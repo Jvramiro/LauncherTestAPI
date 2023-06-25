1a6b5930362cbd440d532d72f598a9a8f6fd2882
João Victor Ramiro <85127729+Jvramiro@users.noreply.github.com>
2023-06-25 13:53:28 -0300

    feat: Placeholder Launcher Context
    Created a temporary Context as a Singleton to suply the needs of the API to perform the tests without the need for a hosted database

---
+++ Data/LauncherContext.cs
@@ -0,0 +1,38 @@
+﻿using LauncherTestAPI.Models;
+using System.Collections.Generic;
+
+namespace LauncherTestAPI.Data {
+    public class LauncherContext {
+
+        private readonly List<Launcher> _launchers;
+        public LauncherContext() {
+            _launchers = new List<Launcher>();
+        }
+
+        public IEnumerable<Launcher> GetAllLaunchers() {
+            return _launchers;
+        }
+
+        public Launcher GetLauncherById(Guid id) {
+            return _launchers.FirstOrDefault(l => l.Id == id);
+        }
+
+        public void AddLauncher(Launcher launcher) {
+            _launchers.Add(launcher);
+        }
+
+        public void UpdateLauncher(Launcher request) {
+            var launcher = _launchers.FirstOrDefault(l => l.Id == request.Id);
+            if (launcher != null) {
+                launcher.Name = request.Name;
+            }
+        }
+
+        public void DeleteLauncher(Guid id) {
+            var launcher = _launchers.FirstOrDefault(l => l.Id == id);
+            if (launcher != null) {
+                _launchers.Remove(launcher);
+            }
+        }
+    }
+}
